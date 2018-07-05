4cbc8d60351f71149520772640abbd4e3a91f0f4
pjay79 <praveen.jayarajan@gmail.com>
2018-07-05 18:18:14 +0800

    Style photos

--- app/screens/PhotosScreen.js
+++ app/screens/PhotosScreen.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, CameraRoll } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Image,
+  TouchableHighlight,
+  StyleSheet,
+  CameraRoll,
+  Dimensions,
+} from 'react-native';
 import Button from '../components/Button';
+
+const { width } = Dimensions.get('window');
 
 export default class PhotosScreen extends Component {
   static navigationOptions = {
@@ -15,7 +25,7 @@ export default class PhotosScreen extends Component {
   };
 
   getPhotos = () => {
-    CameraRoll.getPhotos({ first: 20, assetType: 'All' }).then(
+    CameraRoll.getPhotos({ first: 50, assetType: 'All' }).then(
       (result) => {
         this.setState(
           {
@@ -34,8 +44,21 @@ export default class PhotosScreen extends Component {
   };
 
   render() {
+    const { photos } = this.state;
     return (
       <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          {photos.map((p, i) => (
+            <TouchableHighlight
+              style={{ opacity: i === this.state.index ? 0.5 : 1 }}
+              key={i}
+              underlayColor="transparent"
+              onPress={() => this.setIndex(i)}
+            >
+              <Image style={styles.image} source={{ uri: p.node.image.uri }} />
+            </TouchableHighlight>
+          ))}
+        </ScrollView>
         <Button title="View Photos" style={{ backgroundColor: 'black' }} onPress={this.getPhotos} />
       </View>
     );
@@ -47,5 +70,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
+  },
+  scrollView: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  image: {
+    width: width / 3,
+    height: width / 3,
+    borderColor: 'lightseagreen',
+    borderWidth: 1,
   },
 });
